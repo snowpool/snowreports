80758e2eb23327dab3f18f2689b5ebfc29fa768f
Patrick Davey <patrick.davey@gmail.com>
2019-05-13 13:55:05 +0100

    update link to endpoint

--- spec/snowreports_spec.rb
+++ spec/snowreports_spec.rb
@@ -3,5 +3,7 @@ require "spec_helper"
 RSpec.describe Snowreports do
   it "has a version number" do
     expect(Snowreports::VERSION).not_to be nil
+    Snowreports::Fetcher.user = "api@snowpool.org"
+    Snowreports::Fetcher.pass = "eBFPrRNCZ93afsZU"
   end
 end
